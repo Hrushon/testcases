401c2d52fc4a3dd3b545ca0613eecdb7802f82de
Ilya Steblyan <102170248+Hrushon@users.noreply.github.com>
2023-03-25 13:42:27 +0600

    Fixed(deploy): install gunicorn (#5)

--- testcases/testcases/settings.py
+++ testcases/testcases/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k5gov!n^c^s4(k9%t1%!wzo#kt&34#l=n@k$kwj6&mk^cb%yy0'
+SECRET_KEY = os.getenv('SECRET_KEY', default='django-insecure-k5gov!n^c^s4(k9%t1%!wzo#kt&34#l=n@k$kwj6&mk^cb%yy0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
